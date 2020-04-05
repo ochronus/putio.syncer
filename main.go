16eba76cae131d9a11e226ed608f42cebc2a6ec6
ochronus <ochronus@ochronus.com>
2020-04-05 17:38:05 +0200

    init

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/putdotio/go-putio"
+	"golang.org/x/oauth2"
+)
+
+func main() {
+	godotenv.Load()
+	token := os.Getenv("PUTIO_TOKEN")
+	if len(token) == 0 {
+		panic("PUTIO_TOKEN is empty")
+	}
+	tokenSource := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	oauthClient := oauth2.NewClient(context.TODO(), tokenSource)
+	client := putio.NewClient(oauthClient)
+
+	// get root directory
+	root, err := client.Files.Get(context.TODO(), 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Name of root folder is: %s\n", root.Name)
+}
